324ca651d665442660fd7ef6279e58b59cc47b8f
hsasaki12 <hirotomo.sasaki12@gmail.com>
2023-08-26 15:06:33 +0900

    Add RuboCop

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,11 +1,13 @@
-Rails.application.routes.draw do
-devise_for :users, skip: [:sessions, :registrations]
+# frozen_string_literal: true
 
- as :user do
-  get 'signin', to: 'devise/sessions#new', as: :new_user_session
-  post 'signin', to: 'devise/sessions#create', as: :user_session
-  delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
-  get 'signup', to: 'devise/registrations#new', as: :new_user_registration
-  post 'signup', to: 'devise/registrations#create', as: :user_registration
- end
+Rails.application.routes.draw do
+  devise_for :users, skip: %i[sessions registrations]
+
+  as :user do
+    get 'signin', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
+    post 'signup', to: 'devise/registrations#create', as: :user_registration
+  end
 end
