82f37bd3ef589d531d033c643648ca50a415d43c
hsasaki12 <hirotomo.sasaki12@gmail.com>
2023-08-28 18:57:05 +0900

    create test date GemFaker db:seed

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# ユーザー
+10.times do |_n|
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+# クエスト
+20.times do
+  Quest.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    requester_id: User.pluck(:id).sample,
+    category: rand(1..5),
+    status: rand(1..3),
+    difficulty: rand(1..3)
+  )
+end
