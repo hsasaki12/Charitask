fe47f9aa586da32742c405846b111a7c7a480eb9
hsasaki12 <hirotomo.sasaki12@gmail.com>
2023-09-02 23:04:02 +0900

    User Page Update

--- src/app/controllers/accounts_controller.rb
+++ src/app/controllers/accounts_controller.rb
@@ -3,5 +3,12 @@ class AccountsController < ApplicationController
 
   def show
     @user = current_user
+    @created_quests = Quest.where(requester_id: current_user.id)
+    @accepted_quests = Quest.where(acceptor_id: current_user.id)
+
+    # メッセージの送受信が発生しているクエスト
+    @messaged_quests = Quest.joins(:messages)
+        .where("messages.sender_id = ? OR messages.receiver_id = ?", current_user.id, current_user.id)
+        .distinct
   end
 end
