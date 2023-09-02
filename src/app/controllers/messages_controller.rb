b893eab50f1c94d0b60d3a9c4216930efacf9786
hsasaki12 <hirotomo.sasaki12@gmail.com>
2023-09-02 14:03:22 +0900

    MessageController

--- src/app/controllers/messages_controller.rb
+++ src/app/controllers/messages_controller.rb
@@ -11,15 +11,27 @@ class MessagesController < ApplicationController
     end
   
     def create
-      @message = @quest.messages.build(message_params)
-      @message.sender = current_user
-      @message.receiver = @quest.requester_id == current_user.id ? User.find(@quest.acceptor_id) : User.find(@quest.requester_id)
-      if @message.save
-        redirect_to quest_messages_path(@quest)
-      else
-        # エラーハンドリング...
+        @message = Message.new(message_params)
+        @quest = Quest.find(params[:quest_id])  # 既存のQuestを取得
+      
+        @message.quest_id = @quest.id
+      
+        # sender_idとreceiver_idを設定。具体的なロジックはアプリケーションに依存。
+        if current_user.id == @quest.acceptor_id
+          @message.sender_id = @quest.acceptor_id
+          @message.receiver_id = @quest.requester_id
+        else
+          @message.sender_id = @quest.requester_id
+          @message.receiver_id = @quest.acceptor_id
+        end
+      
+        if @message.save
+          # 成功した処理
+          redirect_to quest_messages_path(@quest) 
+        else
+          # 失敗した処理
+        end
       end
-    end
   
     private
   
