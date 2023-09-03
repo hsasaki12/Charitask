4b6ff6907896c994ab88d6fb458c9640b83d3651
hsasaki12 <hirotomo.sasaki12@gmail.com>
2023-09-03 14:14:51 +0900

    Messages status Update

--- src/app/controllers/messages_controller.rb
+++ src/app/controllers/messages_controller.rb
@@ -39,6 +39,34 @@ def create
   end
 end
 
+
+def update_status
+  @quest = Quest.find(params[:quest_id])
+
+  new_status = params[:new_status]
+
+  if current_user.id == @quest.acceptor_id
+    if @quest.status == "実行前" && new_status == "処理中"
+      @quest.update!(status: "処理中")
+    elsif @quest.status == "処理中" && new_status == "終了確認"
+      @quest.update!(status: "終了確認")
+    else
+      # 不正なステータス更新の場合
+    end
+  elsif current_user.id == @quest.requester_id
+    if @quest.status == "実行前" && new_status == "未着手"
+      @quest.update!(status: "未着手")
+    elsif @quest.status == "終了確認" && new_status == "終了"
+      @quest.update!(status: "終了")
+    else
+      # 不正なステータス更新の場合
+    end
+  else
+    # 権限なし
+  end
+
+  redirect_to quest_messages_path(@quest)
+end
   private
 
   def set_quest
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # config/routes.rb
 Rails.application.routes.draw do
   get 'accounts/show'
@@ -18,6 +16,8 @@ Rails.application.routes.draw do
       post 'accept', to: 'status#accept'
     end
 
-    resources :messages, only: %i[index create]
+    resources :messages, only: %i[index create] do
+      post 'update_status', on: :collection  # ここを追加
+    end
   end
 end
